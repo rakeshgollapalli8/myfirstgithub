d32e6eebe2da429a3ddf123336be76f0b2480640
rakeshgollapalli8 <rakesh.gollapalli5@gmail.com>
2025-11-05 14:31:21 +0530

    Add files via upload

---
+++ DATATYPES.py
@@ -0,0 +1,54 @@
+a=10
+b=20.5
+c=3+5j      
+d="Hello"
+e=True      
+f=None  
+g=[1, 2, 3, 4, 5]
+h=(10, 20, 30)
+i={1, 2, 3, 4, 5}
+j={"name": "Alice", "age": 25}
+k=range(1, 11)
+l=bytes(5)
+m=bytearray(5)
+n=memoryview(b'Hello')
+o=frozenset([1, 2, 3, 4, 5])
+p=lambda x: x * 2
+q=type(a)
+r=isinstance(a, int)
+s=id(a)
+t=dir(a)
+u=hash(a)
+v=help(str)
+print(type(a))
+print(type(b)) 
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+
+
+    
+    
+    
+    
+    
+        
+            
+    
+    
+        
+        
+    
+    
+        
+        
+    
+    
